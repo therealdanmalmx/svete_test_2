38f4f0e7662beebd417b51d8e8dbec01eb70e626
therealdanmalmx <danmalmx@gmail.com>
2023-04-26 23:27:29 +0200

    Stores

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,20 @@
 <script>
+  import { onDestroy } from 'svelte';
+    import countStores from './stores/count.store';
+    const unsubscribe = countStores.subscribe(value => {
+        console.log(value, 'count store');
+    });
+
+    setTimeout(() => {
+        countStores.update(value => {
+            return value + 300
+        });
+    }, 1000);
+
+    onDestroy(() => {
+        unsubscribe();
+    });
 
 </script>
 
+<h1>{$countStores}</h1>
